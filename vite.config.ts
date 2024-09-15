26fad003f6484c9f0698883ff80551992db5ed8c
Manish <[manishbulchandani4@gmail.com]>
2024-09-15 16:23:18 +0530

    tsconfig updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host:"0.0.0.0"
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
